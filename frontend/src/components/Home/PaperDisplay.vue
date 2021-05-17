2b3ae368bfa3360e722fb0b476cafbe88757ebaf
luoao991111 <1714204619@qq.com>
2021-05-17 17:32:39 +0800

    paper display block

---
+++ frontend/src/components/Home/PaperDisplay.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "PaperDisplay"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/views/AllGenres.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "AllGenres"
+}
+</script>
+
+<style scoped>
+
+</style>
