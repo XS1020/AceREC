f68589dc794d7aac702473d16b7c0fdbd348ff3d
zengkaipeng <zkp0819@126.com>
2021-05-24 22:25:16 +0800

    Search Project Start

--- backend/Const_Var.py
+++ backend/Const_Var.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pickle
 
 from backend.settings import BASE_DIR
 
@@ -7,5 +8,7 @@ Mapping_Path = os.path.join(BASE_DIR, 'mapping.json')
 
 Paper_Pdf_Mapping = {}
 with open(Mapping_Path) as Fin:
-	Paper_Pdf_Mapping = json.load(Fin)
-	
+    Paper_Pdf_Mapping = json.load(Fin)
+
+with open('Paper_Subset.pkl', 'rb') as Fin:
+    Paper_Subset = pickle.load(Fin)
