967989e80432594472b5a0959019edd4f2a7b52e
XS <xusheng1020@qq.com>
2021-05-28 20:59:16 +0800

    User Backend Settings

--- backend/User/models.py
+++ backend/User/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Author_INFO(models.Model):
+    local_id = models.AutoField(primary_key=True)
+    remote_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=30, db_index=True)
+    user_name = models.CharField(max_length=30, unique=True)
+    research_list = models.CharField(max_length=200)
+    password = models.CharField(max_length=50)
