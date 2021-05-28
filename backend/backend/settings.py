9632235913259d556007ed9ab0ac1ebfe2e9c751
zengkaipeng <zkp0819@126.com>
2021-05-28 21:14:02 +0800

    test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apis.apps.ApisConfig'
+    'apis.apps.ApisConfig',
+    'User.apps.UserConfig'
 ]
 
 MIDDLEWARE = [
