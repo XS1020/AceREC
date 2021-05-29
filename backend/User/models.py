5aa9d7089be38f523225586f96f918e09f0bbef0
XS <xusheng1020@qq.com>
2021-05-29 13:53:52 +0800

    User Database v2

--- backend/User/models.py
+++ backend/User/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
+# To migrate Models
+# > python manage.py makemigrations User
+# > python manage.py migrate User
+
 # Create your models here.
 
 class User_Info(models.Model):
-    local_id = models.AutoField(primary_key=True)
-    remote_id = models.IntegerField(unique=True)
+    local_id = models.IntegerField(primary_key=True)
+    remote_id = models.BigIntegerField(unique=True)
     name = models.CharField(max_length=30, db_index=True)
     user_name = models.CharField(max_length=30, unique=True)
     affiliation = models.CharField(max_length=50)
@@ -14,7 +18,7 @@ class User_Info(models.Model):
 
 class User_Edu(models.Model):
     local_id = models.IntegerField(db_index=True)
-    remote_id = models.IntegerField(db_index=True)
+    remote_id = models.BigIntegerField(db_index=True)
     year = models.IntegerField()
     action = models.CharField(max_length=20)
     institute = models.CharField(max_length=50)
@@ -22,7 +26,7 @@ class User_Edu(models.Model):
 
 class User_Work(models.Model):
     local_id = models.IntegerField(db_index=True)
-    remote_id = models.IntegerField(db_index=True)
+    remote_id = models.BigIntegerField(db_index=True)
     year = models.IntegerField()
     action = models.CharField(max_length=20)
     institute = models.CharField(max_length=50)
@@ -30,8 +34,8 @@ class User_Work(models.Model):
 
 class User_Interest(models.Model):
     local_id = models.IntegerField(db_index=True)
-    remote_id = models.IntegerField(db_index=True)
-    interest_field = models.CharField(max_length=30)
+    remote_id = models.BigIntegerField(db_index=True)
+    interest_field = models.IntegerField()
 
 class User_Token(models.Model):
     local_id = models.IntegerField(primary_key=True)
@@ -39,5 +43,5 @@ class User_Token(models.Model):
 
 class User_Papers(models.Model):
     local_id = models.IntegerField(db_index=True)
-    remote_id = models.IntegerField(db_index=True)
-    paper_id = models.IntegerField()
+    remote_id = models.BigIntegerField(db_index=True)
+    paper_id = models.BigIntegerField(db_index=True)
