ff1d86aa114a3e17ab358103848078a76a05ce58
zengkaipeng <zkp0819@126.com>
2021-06-02 01:25:55 +0800

    Fuck!!!

--- backend/Front/models.py
+++ backend/Front/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cite_Rec_Cache(models.Model):
+    paper_id = models.BigIntegerField(db_index=True)
+    field_id = models.IntegerField(db_index=True)
+    Update_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+    	ordering = ['Update_time']
+    	
